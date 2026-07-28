e94e6084e657eda9bc5e191a288b147b938c0057
Adarsh-bhandari1 <adarshsinghbhandari@gmail.com>
2026-07-28 16:05:24 +0530

    Stage 1: signup and login routes working

--- Week4/server.js
+++ Week4/server.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 import "dotenv/config";
 const app = express();
 const port = process.env.PORT;
@@ -10,17 +10,58 @@ app.use(express.json());
 const superbase_url = process.env.SUPERBASE_URl;
 const superbase_key = process.env.SUPERBASE_KEY;
 if (!superbase_url || superbase_key) {
-    console.log("Unable to connect to superbase please check the credentials!");
+  console.log("Unable to connect to superbase please check the credentials!");
 }
 
 const superbase = createClient(superbase_url, superbase_key);
-app.get('/', (req, res) => {
-    res.json({
-        message: " Server running fine!"
-    })
+app.get("/", (req, res) => {
+  res.json({
+    message: " Server running fine!",
+  });
+});
+app.post("/auth/signup", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({
+      error: "Please Enter the credentials!",
+    });
+  }
+  try {
+    const { data, error } = await superbase.auth.signUp({
+      email: email,
+      password: password,
+    });
+    if (error) {
+      return res.status(400).json(error);
+    }
+    return res.status(201).json(data);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 });
 
+app.post("/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({
+      error: "Please Enter the credentials!",
+    });
+  }
+  try {
+    const { data, error } = await superbase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+    if (error) {
+      return res.status(401).json(error);
+    }
+
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
 app.listen(port, () => {
-    console.log(`Server running at ${port}`);
-    console.log(`superbase connected`);
-})
+  console.log(`Server running at ${port}`);
+  console.log(`superbase connected`);
+});
